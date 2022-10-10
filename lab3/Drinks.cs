f93a6551aece177528af6f5b2b94209a830f655f
nyamick <nyamick123@gmail.com>
2022-10-10 12:54:46 +0800

    All Drinks classes

--- lab3/Drinks.cs
+++ lab3/Drinks.cs
@@ -16,4 +16,63 @@ namespace lab3
 
         }
     }
+    public enum FruitType { Apple, Orange };
+    public class Juice : Drink
+
+    {
+
+        public FruitType type = FruitType.Apple;
+        public bool Pulp = false;
+
+        public override string GetInfo()
+        {
+            var str = "Cок";
+            str += base.GetInfo();
+            str += String.Format("\n Наличие мякоти: {0}", this.Pulp);
+            str += String.Format("\n Тип фркута: {0}", this.type);
+
+            return str;
+
+        }
+    }
+
+    public enum SodaType { Cola, Sprite };
+    public class Soda : Drink
+    {
+
+        public SodaType type = SodaType.Sprite;
+        public int Bubbles = 0;
+
+        public override string GetInfo()
+        {
+            var str = "Газировка";
+            str += base.GetInfo();
+            str += String.Format("\n Количетсво пузырьков: {0}", this.Bubbles);
+            str += String.Format("\n Тип газировки: {0}", this.type);
+
+            return str;
+
+        }
+
+    }
+
+    public enum AlcType { Beer, Vodka }
+    public class Alcohol : Drink
+    {
+
+        public int Strength = 0;
+        public AlcType type = AlcType.Beer;
+
+        public override string GetInfo()
+        {
+            var str = "Алкоголь";
+            str += base.GetInfo();
+            str += String.Format("\n Крепость: {0}", this.Strength);
+            str += String.Format("\n Тип алкоголя: {0}", this.type);
+
+            return str;
+        }
+
+    }
+}
 }
